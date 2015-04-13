b0fe6219c35f430b04a0d00a20ddec3f309e1d85
Mantas Indrašius <mantasi@wix.com>
2015-04-13 22:06:40 +0300

    Fixing the matcher for contract test

--- client/spec/matchers.js
+++ client/spec/matchers.js
@@ -4,14 +4,21 @@ function configureChai(chai, _) {
     chai.Assertion.addMethod('haveNamesFrom', function (expectedNames) {
         let actualNames = this._obj.map(i => i.name);
 
-        expect(actualNames).to.containAllOf(expectedNames);
+        expect(actualNames).to.containValuesFrom(expectedNames);
     });
 
-    chai.Assertion.addMethod('containAllOf', function (expectedValues) {
+    chai.Assertion.addMethod('containValuesFrom', function (expectedValues) {
         let actualValues = this._obj;
         let intersection = _.intersection(actualValues, _.difference(actualValues, expectedValues));
 
         expect(intersection.length).to.be.equal(0);
+    });
+
+    chai.Assertion.addMethod('containAllOf', function (expectedValues) {
+        let actualValues = this._obj;
+        let intersection = _.intersection(actualValues, expectedValues);
+
+        expect(expectedValues).to.eql(intersection);
     });
 
     chai.Assertion.addMethod('haveSkus', function (expected) {
